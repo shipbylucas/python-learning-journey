420d26ab99fc77003e3df2878fb6a2740dfa9206
Hoang Long <bhlong1911@gmail.com>
2026-06-08 23:54:21 +0700

    Day 15: CS50P PS6 4/4

---
+++ week-2/problem-set-6/shirt.py
@@ -0,0 +1,30 @@
+import sys
+from PIL import Image, ImageOps
+
+if len(sys.argv) in [1, 2]:
+    sys.exit("Too few command-line arguments")
+elif len(sys.argv) > 3:
+    sys.exit("Too many command-line arguments")
+elif len(sys.argv) == 3:
+    if sys.argv[1].lower().endswith((".jpg", ".jpeg", ".png")) == False:
+        sys.exit("Invalid input")
+    elif sys.argv[2].lower().endswith((".jpg", ".jpeg", ".png")) == False:
+        sys.exit("Invalid output")
+    elif sys.argv[1][-4:] != sys.argv[2][-4:]:
+        sys.exit("Input and output have different extensions")
+    elif sys.argv[1][-4:] == sys.argv[2][-4:]:
+        try:
+            with Image.open(sys.argv[1]) as input, Image.open("shirt.png") as shirt:
+                input = input.crop((0, 400, 1200, 1600))
+                input = input.resize((600, 600))
+                resize = ImageOps.fit(
+                    shirt,
+                    input.size,
+                    method=Image.Resampling.BICUBIC,
+                    bleed=0.0,
+                    centering=(0.5, 0.5)
+                )
+                input.paste(resize, (0,0), resize)
+                input.save(sys.argv[2])
+        except FileNotFoundError:
+            sys.exit("Input does not exist")
